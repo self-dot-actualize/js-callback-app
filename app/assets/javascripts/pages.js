e030bd05b3366194eb9ffc10e1325ba0cce15efb
Peter Jang <peter.x.jang@gmail.com>
2017-04-20 20:09:43 -0500

    Change p tags with delay (using nested callbacks)

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,9 +1,14 @@
 function changeColors() {
   setTimeout(function() {
     document.querySelector("#first").style["color"] = "red";
+    setTimeout(function() {
+      document.querySelector("#second").style["color"] = "blue";
+      setTimeout(function() {
+        document.querySelector("#third").style["color"] = "green";
+        setTimeout(function() {
+          document.querySelector("#fourth").style["color"] = "orange";
+        }, 2000);
+      }, 2000);
+    }, 2000);
   }, 2000);
-
-  setTimeout(function() {
-    document.querySelector("#second").style["color"] = "blue";
-  }, 4000);
 }
