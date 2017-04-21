4d425fc534303a9a8c9acd2cbb71d4b11f319fcb
Peter Jang <peter.x.jang@gmail.com>
2017-04-20 21:20:06 -0500

    Add more detailed example to put http response into the DOM

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,5 +1,13 @@
 /* global $ */
 $.get('https://data.cityofchicago.org/resource/xzkq-xp2w.json', function(data) {
-  console.log(data);
-  document.querySelector("#data").innerHTML = data;
+  var htmlString = "";
+  for (var i = 0; i < data.length; i++) {
+    htmlString += '<h2 onmousedown="changeEmployee(this)">' + data[i].name + '</h2>';
+    htmlString += '<p>' + data[i].department + '</p>';
+  }
+  document.querySelector("#data").innerHTML = htmlString;
 });
+
+function changeEmployee(headerTag) {
+  headerTag.style["color"] = "gray";
+}
