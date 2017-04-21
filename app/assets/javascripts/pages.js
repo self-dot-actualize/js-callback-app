7423f79a5bf06859c500583bad450c7a2887ae26
Peter Jang <peter.x.jang@gmail.com>
2017-04-20 20:35:30 -0500

    Add basic example to put http response into the DOM

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,23 +1,5 @@
-function changeColors() {
-  setTimeout(function() {
-    document.querySelector("#first").style["color"] = "red";
-    setTimeout(function() {
-      document.querySelector("#second").style["color"] = "blue";
-      setTimeout(function() {
-        document.querySelector("#third").style["color"] = "green";
-        setTimeout(function() {
-          document.querySelector("#fourth").style["color"] = "orange";
-        }, 2000);
-      }, 2000);
-    }, 2000);
-  }, 2000);
-}
-
-// WRONG WAY
-// var response = $.get('https://data.cityofchicago.org/resource/xzkq-xp2w.json');
-// console.log(response);
-
-// RIGHT WAY
+/* global $ */
 $.get('https://data.cityofchicago.org/resource/xzkq-xp2w.json', function(data) {
   console.log(data);
+  document.querySelector("#data").innerHTML = data;
 });
